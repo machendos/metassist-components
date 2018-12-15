9cde1948bbe832edc009609d0e854f4b404df3fc
Nicita01 <nicitamac@gmail.com>
2018-12-15 16:34:40 +0200

    Remake function-constructor to class

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Checkbox(props) {
-  return (
-    <div>
-      <input type="checkbox" id="first"/>
-      <label for="first">{props.name}</label>
-    </div>
-  )
+class Checkbox extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      checked: false
+    }
+    this.caption = props.caption;
+    this.data = props.data;
+  }
+
+  render() {
+    console.log.call(this, this.state.open);
+    return (
+      <div>
+        <input type="checkbox" id="first" onClick = {this.clickReaction} />
+        <label htmlFor="first">{this.caption}</label><br />
+        
+      </div>
+    )
+  }
+
 }
+
 
 export default Checkbox;
