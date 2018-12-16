d5aa1ab12d71d6620a6c0d088a4843495f757ea2
Nicita01 <nicitamac@gmail.com>
2018-12-16 11:02:12 +0300

    Fix App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Checkbox from './Checkbox';
+import DayInput from './DayInput';
 
 function App() {
   return (
     <div>
-      <Checkbox caption="wont to choose a day?" data={['aaa', 'bbb', 'ccc']} />
+      <Checkbox caption="wont to choose a day?" data={[<DayInput day="monday" />, <DayInput day="tuesday"/>]} />
     </div>
   )
 }
