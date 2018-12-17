b2e5c51b3dfe2141c7f3b25e05bd42c22dcca703
Nicita01 <nicitamac@gmail.com>
2018-12-17 15:23:54 +0300

    Fix Time.jsx

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 class Time extends Component {
+  constructor(props) {
+    super(props);
+    this.day = props.day;
+    this.newId = props.newId;
+  }
+
   render() {
+    const id1 = this.day + this.newId;
+    const id2 = this.day + (this.newId + 1);
     return (
       <div>
-      <input type="text"></input>
-      :
-      <input type="text"></input>
+        <input id={id1} type="text"></input>
+        :
+        <input id={id2} type="text"></input>
       </div>
     )
   }
